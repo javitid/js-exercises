61d9ce98fe95b713159301a07f74463fe8884374
Javier Garcia <javitid@gmail.com>
2021-11-21 11:05:36 +0100

    Add Big O notation comments

--- README.md
+++ README.md
@@ -2,6 +2,23 @@
 - Tests: `npm run tests`
 - Coverage: `npm run coverage`
 
-Exercise 3 notes:
-- tf_idf.js is the main file and it should have permission to execution: `chmod +x tf.idf.js`
-- Example of execution: `./td_idf.js -d dir -n 5 -p 1000 -t "term1 term2 ..."`
+Exercise 1 - Palindrome:
+    - isPalindrome -> O(n^2)
+    - isPalindromeEfficient -> O(n/2)
+    Note: it was not in the spaces but I added in both methods a way to "Remove spaces O(n) and convert in lowercase O(n)" that are adding extra complexity
+
+Exercise 2 - K-complementary:
+    - findKComplementary -> O(n^2)
+
+Exercise 3:
+    - processArgs -> O(n)
+    - getTermOccurrenciesFromFiles -> O(n)
+    - getOccurreciesInContent -> O(n)
+    - calculateTfIdf -> O(n)
+    - getFilesWithTerms -> O(n)
+    - addTfIdf -> O(1)
+    - printResult -> O(n)
+
+    Notes:
+        - tf_idf.js is the main file and it should have permission to execution: `chmod +x tf.idf.js`
+        - Example of execution: `./td_idf.js -d dir -n 5 -p 1000 -t "term1 term2 ..."`
--- src/exercise1/palindrome.js
+++ src/exercise1/palindrome.js
@@ -1,19 +1,19 @@
-const SPACE = /\s/g;
+const SPACE_REGEX = /\s/g;
 const EMPTY_STRING = '';
 
-// Option 1, shorter code
-// Remove spaces, convert in lowercase
+// Option 1, shorter code O(n^2)
+// Remove spaces, convert in lowercase O(n) + O(n)
 // Convert to array, reverse it, convert to string and compare with the given string
 function isPalindrome(value) {
-    value = value.replace(SPACE, EMPTY_STRING).toLowerCase();
+    value = value.replace(SPACE_REGEX, EMPTY_STRING).toLowerCase();
     return value === value.split(EMPTY_STRING).reverse().join(EMPTY_STRING);
 }
 
-// Option 2, more performance
-// Remove spaces, convert in lowercase
+// Option 2, more performance O(n/2)
+// Remove spaces, convert in lowercase O(n) + O(n)
 // Compare each char with the opposite, only needs to iterate 1/2 of the string and it can response when some compared chars don't match
 function isPalindromeEfficient(value) {
-    value = value.replace(SPACE, EMPTY_STRING).toLowerCase();
+    value = value.replace(SPACE_REGEX, EMPTY_STRING).toLowerCase();
     for (let i=0; i<value.length/2; i++) {
         if (value[i] !== value[value.length - i - 1]) {
             return false;
