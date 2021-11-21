d0b85b96352ba81a4faa1cdc05754ff1ca4f44de
Javier Garcia <javitid@gmail.com>
2021-11-21 13:44:35 +0100

    Update unit tests

--- src/exercise3/__tests__/tf_idf.args.js
+++ src/exercise3/__tests__/tf_idf.args.js
@@ -11,14 +11,26 @@ const {
     addTfIdf
 } = require('../tf_idf.js');
 
+const FAKE_FILE_CONTENT_SHORT = 'this is a hello world sentence.';
+const FAKE_FILE_CONTENT_LONG = 'this is a hello world sentence. And this is a hello';
+
 describe('getOccurreciesInContent', () => {
-    it('should return the number of occurencies of single term in content', () => {
-        const occurrecies = getOccurreciesInContent('hello', 'this is a hello world sentence. And this is a hello')
+    beforeAll(done => {
+        done();
+    })
+
+    it('should find the number of occurencies of single term in content', () => {
+        const occurrecies = getOccurreciesInContent('hello', FAKE_FILE_CONTENT_LONG);
         expect(occurrecies).toBe(2);
     });
 
-    it('should return the number of occurencies of complex term in content', () => {
-        const occurrecies = getOccurreciesInContent('hello world', 'this is a hello world sentence')
+    it('should find the number of occurencies of complex term in content', () => {
+        const occurrecies = getOccurreciesInContent('hello world', FAKE_FILE_CONTENT_SHORT);
+        expect(occurrecies).toBe(1);
+    });
+
+    it('should not be case sensitive', () => {
+        const occurrecies = getOccurreciesInContent('Hello', FAKE_FILE_CONTENT_SHORT);
         expect(occurrecies).toBe(1);
     });
 });
