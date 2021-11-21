f8f9ae14e1a566f60f59f2bef5c19d7b0d2476f5
Javier Garcia <javitid@gmail.com>
2021-11-21 12:10:05 +0100

    Add unit tests

---
+++ src/exercise3/__tests__/process_args.specs.js
@@ -0,0 +1,95 @@
+const mockExit = jest.spyOn(process, 'exit').mockImplementation(() => {});
+const {processArgs} = require('../process_args');
+const {DEFAULT_VALUES} = require('../constants.js');
+
+describe('processArgs', () => {
+    describe('should retrieve input parameters', () => {
+        it('directory d', () => {
+            const args = '-d directory';
+
+            const {d} = processArgs(args.split(' '));
+            expect(d).toBe('directory');
+        });
+
+        it('period p', () => {
+            const args = '-p 1000';
+
+            const {p} = processArgs(args.split(' '));
+            expect(p).toBe(1000);
+        });
+
+        it('numbers top n', () => {
+            const args = '-n 3';
+
+            const {n} = processArgs(args.split(' '));
+            expect(n).toBe(3);
+        });
+
+        it('terms t', () => {
+            const args = '-t "term1"';
+
+            const {t} = processArgs(args.split(' '));
+            expect(t).toBe('"term1"');
+        });
+
+        it('several terms in the same line', () => {
+            const args = '-d directory -p 1000 -n 3 -t "term1"';
+
+            const {d, t, n, p} = processArgs(args.split(' '));
+            expect(d).toBe('directory');
+            expect(p).toBe(1000);
+            expect(n).toBe(3);
+            expect(t).toBe('"term1"');
+        });
+
+        it('and support capitol letters', () => {
+            const args = '-D directory -P 1000 -N 3 -T "term1"';
+
+            const {d, t, n, p} = processArgs(args.split(' '));
+            expect(d).toBe('directory');
+            expect(p).toBe(1000);
+            expect(n).toBe(3);
+            expect(t).toBe('"term1"');
+        });
+    });
+
+    describe('should set default values if there are not in the input', () => {
+        it('directory d', () => {
+            const {d} = processArgs([]);
+            expect(d).toBe(DEFAULT_VALUES.DIRECTORY);
+        });
+
+        it('period p', () => {
+            const {p} = processArgs([]);
+            expect(p).toBe(DEFAULT_VALUES.PERIOD);
+        });
+
+        it('numbers top n', () => {
+            const {n} = processArgs([]);
+            expect(n).toBe(DEFAULT_VALUES.NUMBERS_TOP);
+        });
+    });
+
+    describe('should exit', () => {
+        it('when numbers top n is not a number', () => {
+            const args = '-n text';
+
+            processArgs(args.split(' '));
+            expect(mockExit).toHaveBeenCalled();
+        });
+
+        it('when period p is not a number', () => {
+            const args = '-p text';
+
+            processArgs(args.split(' '));
+            expect(mockExit).toHaveBeenCalled();
+        });
+
+        it('when there is no term string', () => {
+            const args = '-d directory -p 1000 -n 3';
+
+            processArgs(args.split(' '));
+            expect(mockExit).toHaveBeenCalled();
+        });
+    });
+});
--- src/exercise3/process_args.js
+++ src/exercise3/process_args.js
@@ -5,11 +5,12 @@ const {
 } = require('./constants.js');
 
 // Set parameters to an object with the input data
-function processArgs() {
-    var d='', t=[], n='', p='';
+function processArgs(args) {
+    var d='', t='', n='', p='';
     // Discard 2 first arguments because they are paths
     // Set parameters to uppercase, keep the values as they are in the input
-    var args = process.argv.slice(2).map(argument => (argument[0] === INDEX.ARGUMENT) ? argument.toUpperCase() : argument);
+    var args = args || process.argv.slice(2);
+    args = args.map(argument => (argument[0] === INDEX.ARGUMENT) ? argument.toUpperCase() : argument);
     
     args.forEach((argument, index) => {
         const isOption = argument[0] === INDEX.ARGUMENT;
@@ -24,10 +25,10 @@ function processArgs() {
                     t = next;
                     break;
                 case PARAMETERS.NUMBER_TOP:
-                    n = parseInt(next);
+                    n = next;
                     break;
                 case PARAMETERS.PERIOD:
-                    p = parseInt(next);
+                    p = next;
                     break;
                 default:
                     console.log('Wrong parameter: ' + args[index]);
@@ -44,14 +45,21 @@ function processArgs() {
     // Validations
     if (typeof(n) != 'number') {
         console.log('Number of top elements "n" should be a number.');
+        process.exit();
     }
     if (typeof(p) != 'number') {
         console.log('Period "p" should be a number.');
+        process.exit();
     }
     if (t === '') {
         console.log('Parameter "t" is mandatory. Terms to be analyzed.');
         console.log('Example of execution: ./td-idf.js -d dir -n 5 -p 1000 -t "term1 term2 ..."');
+        process.exit();
     }
+
+    // Set n and p as int values
+    n = parseInt(n);
+    p = parseInt(p);
 
     return {d, t, n, p};
 }
